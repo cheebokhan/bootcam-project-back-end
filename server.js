b410e7641a89e9b7dcf35062368f4b3942ad0d00
Haseeb Ur Rehman <100108916+cheebokhan@users.noreply.github.com>
2023-04-07 14:44:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/books', BookRouter);
 app.use(error.errorHandlerMiddleware);
 
 //Server
-const PORT = 4000;
+const PORT =  process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is up and runing ${PORT}`);
 });
